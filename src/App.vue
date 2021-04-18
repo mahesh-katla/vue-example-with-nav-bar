8b4c5aefaf0da832fd3f7c995177f112ea5a3156
mahesh-katla <mahesh143k@gmail.com>
2021-04-19 00:11:06 +0530

    nav-bar

--- src/App.vue
+++ src/App.vue
@@ -2,42 +2,57 @@
   <v-app>
     <v-app-bar
       app
-      color="primary"
+      color="red"
       dark
     >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
+      
       <v-spacer></v-spacer>
 
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
+      <v-menu
+            top
+            :close-on-click="closeOnClick"
+          >
+            <template v-slot:activator="{ on, attrs }">
+              <v-btn
+                icon
+                v-bind="attrs"
+                v-on="on"
+              >
+                <v-icon>mdi-account-circle</v-icon>
+              </v-btn>
+            </template>
+
+            <v-list>
+              <v-list-item
+                v-for="(item, index) in  menuItems"
+                :key="index"
+              >
+                <v-list-item-title>{{ item.title }}</v-list-item-title>
+              </v-list-item>
+            </v-list>
+          </v-menu>
+
     </v-app-bar>
 
+
+
+
     <v-main>
+
+    <v-toolbar>
+
+        <v-btn text >
+          Menu 1
+        </v-btn>
+
+        <v-btn text >
+              Menu 2
+            </v-btn>
+
+       </v-toolbar>
+
+     
+
       <HelloWorld/>
     </v-main>
   </v-app>
@@ -53,8 +68,14 @@ export default {
     HelloWorld,
   },
 
+
+
+
   data: () => ({
     //
+    menuItems: [{title: 'Menu1'}, {title: 'Menu 2'}],
+
+
   }),
 };
 </script>
